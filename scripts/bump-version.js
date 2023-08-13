d112cce8a7f7b7ce01599f339b6d5427d4d4a6d5
Nicholas Sylke <nicksylke@outlook.com>
2023-08-13 10:04:19 -0500

    chore: create script to bump version and dependencies

---
+++ scripts/bump-version.js
@@ -0,0 +1,19 @@
+const pkg = require("../package.json");
+const glob = require("glob");
+const fs = require("fs");
+
+glob.sync("./packages/**/package.json").forEach(
+  (loc) =>
+    !loc.includes("node_modules") &&
+    fs.writeFileSync(
+      loc,
+      JSON.stringify(
+        {
+          ...JSON.parse(fs.readFileSync(loc)),
+          version: pkg.version,
+        },
+        null,
+        2
+      )
+    )
+);
