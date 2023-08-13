ad85c2d906a5ec5e2e23396dc796b2537f737e7b
Nicholas Sylke <nicksylke@outlook.com>
2023-08-13 11:51:43 -0500

    feat: create marketing app (#3)

---
+++ apps/marketing/src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <img className="h-7" src="/branding/icon.svg" alt="Trpkit" />
+          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-white">Product</h3>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-white">Comparison</h3>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-white">Legal</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 border-t border-slate-800 pt-8 md:flex md:items-center md:justify-between">
+          <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+            &copy; 2023 Trpkit LLC. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
