b3da0cd6d43a04f85bae4e28cc5a16d2d21741e0
Nicholas Sylke <nicksylke@outlook.com>
2023-08-13 19:55:18 -0500

    feat: create regex for aesgcm256 messages

--- packages/kms/src/types.ts
+++ packages/kms/src/types.ts
@@ -7,6 +7,10 @@ export type CipherEncryptionResult = {
 export const KMSAesGcm256Key = /^kms\/aesgcm256\/(?<key>[a-zA-Z0-9-_]{43}=?)$/;
 export const KMSXChaCha20Poly1305Key = /^kms\/xchacha20poly1305\/(?<key>[a-zA-Z0-9-_]{43}=?)$/;
 
+// Regex for verifying KMS messages
+export const KMSAesGcm256Message =
+  /^kms\/aesgcm256\/(?<fingerprint>[0-9a-fA-F]{8})\/(?<nonce>[a-zA-Z0-9-_]{16})\/(?<cipher>[a-zA-Z0-9-_]{22,})={0,2}$/;
+
 // The formatted key stored in our servers. we do not store the master key, only keychain keys.
 export type KMSKey = string;
 
