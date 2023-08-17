717bea224533c9297edd50148bb9dfc40c4abbc0
Nicholas Sylke <nicksylke@outlook.com>
2023-08-16 20:30:05 -0500

    feat: create paths in marketing tsconfig

--- apps/marketing/src/app/page.tsx
+++ apps/marketing/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Faq from "../components/Faq";
-import Features from "../components/Features";
-import Hero from "../components/Hero";
-import Waitlist from "../components/Waitlist";
+import Faq from "@components/Faq";
+import Features from "@components/Features";
+import Hero from "@components/Hero";
+import Waitlist from "@components/Waitlist";
 
 export default function Page() {
   return (
