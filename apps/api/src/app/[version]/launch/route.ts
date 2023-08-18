1c24da9750446222a56cb4839ab4edc005661f4a
Nicholas Sylke <nicksylke@outlook.com>
2023-08-17 19:31:57 -0500

    feat: create launch route

---
+++ apps/api/src/app/[version]/launch/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { mongo } from "@trpkit/storage";
+
+// Specifying a version in this route is not necessary since its temporary and will be removed at launch
+export async function POST(request: NextRequest) {
+  // Get email address from search parameter (e.g. ?email=john.doe@example.com)
+  const email = request.nextUrl.searchParams.get("email");
+
+  // Error response from no email address
+  if (!email) {
+    return NextResponse.json(
+      {
+        message: "Missing email address from request.",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const client = await mongo();
+  const db = client.db(process.env.MONGO_DATABASE);
+
+  // Submitting the same email more than once will only change the submittedAt field, no need to return an error message if already subscribed.
+  await db
+    .collection("launch")
+    .updateOne(
+      { email: email },
+      { $set: { email: email, submittedAt: Date.now() } },
+      { upsert: true }
+    );
+
+  // Return a created status code
+  return NextResponse.next({ status: 201 });
+}
