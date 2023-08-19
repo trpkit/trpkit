63111fd446bca117a7dd0eb417aeba1e8ff753b0
Nicholas Sylke <nicksylke@outlook.com>
2023-08-19 09:56:31 -0500

    feat: sign a jwt token and pass it into response for signing up

--- apps/api/src/app/auth/register/route.ts
+++ apps/api/src/app/auth/register/route.ts
@@ -1,3 +1,5 @@
+import { utf8 } from "@scure/base";
+import { JWTPayload, SignJWT } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
 import { mongo } from "@trpkit/storage";
@@ -36,6 +38,24 @@ interface KeychainObject {
   public: string;
 }
 
+// TODO: Move this into a separate package with other common utilities
+async function sign(payload: JWTPayload): Promise<string> {
+  return (
+    new SignJWT(payload)
+      // TODO: Switch to RS256, we'll do this before market release, may use HS256 for beta though
+      .setProtectedHeader({
+        alg: "HS256",
+        typ: "JWT",
+      })
+      .setIssuedAt()
+      .setIssuer("urn:trpkit:api.trpkit.com")
+      .setAudience("urn:trpkit:app.trpkit.com")
+      .setExpirationTime("7d")
+      // TODO: We'll probably create a CLI to generate secrets through our KMS during an initialization script for dev environments
+      .sign(utf8.decode(process.env.JWT_SECRET))
+  );
+}
+
 export async function POST(request: NextRequest) {
   const body = (await request.json()) as IncomingBodyRequest;
 
@@ -53,8 +73,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(
       {
         // TODO: Probably should change this error message
-        //  Look into other privacy-focused applications to see what the messaging is like. Ideally, we shouldn't let
-        //  someone know that an account already exists.
+        //  Look into other privacy-focused applications to see what the messaging is like. Ideally, we shouldn't let someone know that an account already exists.
         message: "Email already in use.",
       },
       {
@@ -64,6 +83,7 @@ export async function POST(request: NextRequest) {
   }
 
   // Insert into database
+  // TODO: Once we switch to zod, we can use safeParse to ensure the correct fields are being inserted. This will happen before beta launch.
   await db.collection("users").insertOne({
     firstName: body.firstName,
     lastName: body.lastName,
@@ -81,14 +101,20 @@ export async function POST(request: NextRequest) {
   });
 
   // TODO: Send welcome email
-  //  We could probably personalize this and use Nick or Tyler's email to send it from instead of a generic noreply or
-  //  support address.
+  //  We could probably personalize this and use Nick or Tyler's email to send it from instead of a generic noreply or support address.
 
-  // TODO: Create JWT token and send back to user
-  // You will automatically be logged in after registration instead of having to go and sign in after registration. Lets
-  // provide a seamless flow.
-
-  return new Response(null, {
-    status: 201,
+  // Create JWT token and send back to user
+  // You will automatically be logged in after registration instead of having to go and sign in after registration. Let's provide a seamless flow.
+  const token = await sign({
+    email: body.email,
   });
+
+  return NextResponse.json(
+    {
+      token: token,
+    },
+    {
+      status: 200,
+    }
+  );
 }
