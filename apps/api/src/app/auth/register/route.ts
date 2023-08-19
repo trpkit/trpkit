01e6c3d72739fb55e75fac67d1954b8aab6031b0
Nicholas Sylke <nicksylke@outlook.com>
2023-08-18 22:31:49 -0500

    feat: add typings for incoming registration

--- apps/api/src/app/auth/register/route.ts
+++ apps/api/src/app/auth/register/route.ts
@@ -1,4 +1,37 @@
-export async function POST() {
+import { NextRequest } from "next/server";
+
+interface IncomingBodyRequest {
+  firstName: string;
+  lastName: string;
+  email: string;
+  credentials: IncomingBodyCredentialsRequest[];
+  kms: IncomingBodyKMSRequest;
+  keychain: IncomingBodyKeychainRequest;
+}
+
+interface IncomingBodyCredentialsRequest {
+  [key: string]: string;
+}
+
+interface IncomingBodyKMSRequest {
+  salt: string;
+  recovery: string;
+  keychain: string;
+}
+
+interface IncomingBodyKeychainRequest {
+  signature: IncomingBodyKeychainObject;
+  sharing: IncomingBodyKeychainObject;
+}
+
+interface IncomingBodyKeychainObject {
+  secret: string;
+  public: string;
+}
+
+export async function POST(request: NextRequest) {
+  const body = (await request.json()) as IncomingBodyRequest;
+
   // Handle registration
 
   return new Response(null, {
