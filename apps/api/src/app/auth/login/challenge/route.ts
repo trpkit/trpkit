3ec190eeaaf0a368c05561bb4c0e8cf61d40b10e
Nicholas Sylke <nicksylke@outlook.com>
2023-08-19 08:46:57 -0500

    chore: jot some notes down about challenge route for logging in

--- apps/api/src/app/auth/login/challenge/route.ts
+++ apps/api/src/app/auth/login/challenge/route.ts
@@ -1,6 +1,18 @@
 export async function POST() {
-  // Handle challenge
+  // Client -----> Server
+  // username (email) and clientEphemeral.public
 
+  // TODO: Validate incoming email and ephemeral
+  //  If the user doesn't exist, send bogus salt & ephemeral back
+
+  // TODO: Generate server ephemeral
+
+  // TODO: Store server ephemeral secret in database for later use
+
+  // Client <----- Server
+  // salt and serverEphemeral.public
+
+  // TODO: Send response back to client
   return new Response(null, {
     status: 200,
   });
