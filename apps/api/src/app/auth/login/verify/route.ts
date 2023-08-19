7abf58506f526a44d93f23ed22319e28fa9d338c
Nicholas Sylke <nicksylke@outlook.com>
2023-08-19 13:32:52 -0500

    chore: jot notes down about verify route on logging in

--- apps/api/src/app/auth/login/verify/route.ts
+++ apps/api/src/app/auth/login/verify/route.ts
@@ -1,5 +1,17 @@
 export async function POST() {
-  // Handle verify
+  // Client -----> Server
+  // clientSession.proof
+
+  // TODO: Validate the proof and derive session
+
+  // Client <----- Server
+  // serverSession.proof
+
+  // TODO: Send response back to server
+
+  // TODO: Determine if 2FA is enabled on the account.
+  //  No 2FA -> Send KMS salt, KMS keychain and keychain in response
+  //  2FA    -> Require client to submit 2FA code or passkey, then send KMS salt, KMS keychain and keychain if successful
 
   return new Response(null, {
     status: 200,
