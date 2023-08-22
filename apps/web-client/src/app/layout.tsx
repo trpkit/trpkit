a6494ee8423d2a80340b0c90be985a14e93226bb
Nicholas Sylke <nicksylke@outlook.com>
2023-08-21 20:52:42 -0500

    feat: configure dockerfile, docker-compose, tailwindcss, postcss, next-themes for webclient

---
+++ apps/web-client/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import React from "react";
+
+import Providers from "./providers";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
+      <body className="bg-slate-900">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
