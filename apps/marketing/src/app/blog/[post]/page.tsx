46b8a7c561e177876ca30e928363347df661eb3d
nsylke <nicksylke@outlook.com>
2023-08-24 20:20:01 -0500

    feat: add metadata for blog posts

--- apps/marketing/src/app/blog/[post]/page.tsx
+++ apps/marketing/src/app/blog/[post]/page.tsx
@@ -1,8 +1,38 @@
 import BlogArticle from "@components/BlogArticle";
 import { formatDate } from "@lib/format-date";
 import { allBlogDocuments } from "contentlayer/generated";
+import { Metadata } from "next";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { post: string };
+}): Promise<Metadata | undefined> {
+  const doc = allBlogDocuments.find((doc) => doc.href === params.post);
+
+  if (!doc) {
+    return;
+  }
+
+  return {
+    title: doc.title,
+    description: doc.summary,
+    openGraph: {
+      title: doc.title,
+      description: doc.summary,
+      type: "article",
+      publishedTime: doc.date,
+      url: `https://trpkit.com/${doc.href}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: doc.title,
+      description: doc.summary,
+    },
+  };
+}
 
 export default function Page({ params }: { params: { post: string } }) {
   const doc = allBlogDocuments.find((doc) => doc.href === params.post);
