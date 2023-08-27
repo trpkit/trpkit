d864b5985c7f01599832926910c95a6234f76f62
nsylke <nicksylke@outlook.com>
2023-08-26 21:59:17 -0500

    feat: add login form design

--- apps/web-client/src/app/(auth)/login/page.tsx
+++ apps/web-client/src/app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import LoginForm from "../../../components/(auth)/LoginForm";
+
 export default function Page() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -14,7 +16,7 @@ export default function Page() {
         </h2>
       </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        {/* TODO: Login form */}
+        <LoginForm />
         <p className="mt-10 text-center text-sm text-gray-400">
           Not a member?{" "}
           <Link
---
+++ apps/web-client/src/components/(auth)/LoginForm.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: Client <---> Server handshake
+  };
+
+  return (
+    <form className="space-y-6" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+            Password
+          </label>
+          <div className="text-sm">
+            <a href="#" className="font-semibold text-blue-400 hover:text-blue-300">
+              Forgot password?
+            </a>
+          </div>
+        </div>
+        <div className="mt-2">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">
+          Log into account
+        </button>
+      </div>
+    </form>
+  );
+}
