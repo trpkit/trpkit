cccf54ea4d329ff0e34e146155d69625ab112c89
Nicholas Sylke <nicksylke@outlook.com>
2023-08-28 20:29:54 -0500

    refactor: expand width of landing page (#67)

--- apps/marketing/src/components/Header.tsx
+++ apps/marketing/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <nav className="max-w-screen-2xl mx-auto w-full px-5 flex items-center justify-between p-6">
         <div className="flex flex-1" />
         <Link href="/" className="-m-1.5 p-1.5">
           <span className="sr-only">Trpkit</span>
