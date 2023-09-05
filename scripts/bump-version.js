f9e33442054041afecc4ed19e8705d003bcd9f9f
nsylke <nicksylke@outlook.com>
2023-09-05 18:46:21 -0500

    fix: include apps in versioning script

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -2,6 +2,22 @@ const pkg = require("../package.json");
 const glob = require("glob");
 const fs = require("fs");
 
+glob.sync("./apps/**/package.json").forEach(
+  (loc) =>
+    !loc.includes("node_modules") &&
+    fs.writeFileSync(
+      loc,
+      JSON.stringify(
+        {
+          ...JSON.parse(fs.readFileSync(loc)),
+          version: pkg.version,
+        },
+        null,
+        2
+      )
+    )
+);
+
 glob.sync("./packages/**/package.json").forEach(
   (loc) =>
     !loc.includes("node_modules") &&
