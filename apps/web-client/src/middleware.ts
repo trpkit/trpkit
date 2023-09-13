849404301c6bae409a997b083c06257180a430ce
nsylke <nicksylke@outlook.com>
2023-09-13 15:48:19 -0500

    refactor: implement middleware functionality for authentication

--- apps/web-client/src/middleware.ts
+++ apps/web-client/src/middleware.ts
@@ -1,11 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { verify } from "./lib/jwt";
+
 export const config = {
   matcher: ["/((?!login|register|_next/static|_next/image|favicon.ico).*)"],
 };
 
 export async function middleware(request: NextRequest) {
-  // TODO: Handle middleware
+  if (!request.cookies.has(process.env.NEXT_PUBLIC_JWT_COOKIE_NAME)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
-  return NextResponse.redirect(new URL("/login", request.url));
+  const token = request.cookies.get(process.env.NEXT_PUBLIC_JWT_COOKIE_NAME);
+
+  try {
+    await verify(token.value);
+
+    return NextResponse.next();
+  } catch {
+    request.cookies.clear();
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
