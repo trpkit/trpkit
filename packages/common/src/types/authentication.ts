161f452c874350282635172c353835584def2258
nsylke <nicksylke@outlook.com>
2023-09-23 10:47:01 -0500

    refactor: remove first and last name from registration

--- packages/common/src/types/authentication.ts
+++ packages/common/src/types/authentication.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 
 export const ZAuthRegistration = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
   email: z.string().email(),
   srp: z.object({
     salt: z.string(),
