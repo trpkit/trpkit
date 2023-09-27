0273108817faa3ed6da6a497a2c54c5ff79e0d3c
Nicholas Sylke <nicksylke@outlook.com>
2023-09-26 22:25:13 -0500

    feat: configure tailwind, postcss and basic page for web-client (#123)
    * feat: configure tailwind, postcss and basic page for web-client
    
    * fix: add missing baseUrl to tsconfig

---
+++ apps/web-client/src/app/(dash)/layout.tsx
@@ -0,0 +1,17 @@
+import "@styles/globals.css";
+import { Inter } from "next/font/google";
+import React from "react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <body>{children}</body>
+    </html>
+  );
+}
