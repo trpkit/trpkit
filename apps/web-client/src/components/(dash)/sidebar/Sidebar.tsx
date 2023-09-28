df78a784e2edb3d4fddc79187170d0f1a2baf3a3
nsylke <nicksylke@outlook.com>
2023-09-27 21:01:50 -0500

    refactor: update sidebar navigation items

--- apps/web-client/src/components/(dash)/sidebar/Sidebar.tsx
+++ apps/web-client/src/components/(dash)/sidebar/Sidebar.tsx
@@ -1,4 +1,10 @@
-import { ChartBarIcon, ChevronDownIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
+import {
+  ChartBarIcon,
+  ChevronDownIcon,
+  Cog6ToothIcon,
+  HomeIcon,
+  LockClosedIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default function Sidebar() {
@@ -19,9 +25,31 @@ export default function Sidebar() {
             href="#"
             className="group m-0 flex min-h-[40px] w-full cursor-pointer items-center rounded px-3 text-sm text-white hover:bg-neutral-900 focus:outline-none bg-neutral-900">
             <div className="mr-3 h-5 w-5">
+              <HomeIcon className="h-5 w-5" aria-hidden="true" />
+            </div>
+            <div>Home</div>
+          </a>
+          <a
+            href="#"
+            className="group m-0 flex min-h-[40px] w-full cursor-pointer items-center rounded px-3 text-sm text-gray-300 hover:bg-neutral-900 hover:text-white focus:outline-none">
+            <div className="mr-3 h-5 w-5">
               <ChartBarIcon className="h-5 w-5" aria-hidden="true" />
             </div>
-            <div>Dashboard</div>
+            <div>Analytics</div>
+            <div className="ml-auto h-4 w-4 flex-shrink-0">
+              <ChevronDownIcon className="h-4 w-4" aria-hidden="true" />
+            </div>
+          </a>
+          <a
+            href="#"
+            className="group m-0 flex min-h-[40px] w-full cursor-pointer items-center rounded px-3 text-sm text-gray-300 hover:bg-neutral-900 hover:text-white focus:outline-none">
+            <div className="mr-3 h-5 w-5">
+              <LockClosedIcon className="h-5 w-5" aria-hidden="true" />
+            </div>
+            <div>Security</div>
+            <div className="ml-auto h-4 w-4 flex-shrink-0">
+              <ChevronDownIcon className="h-4 w-4" aria-hidden="true" />
+            </div>
           </a>
           <a
             href="#"
@@ -30,8 +58,8 @@ export default function Sidebar() {
               <Cog6ToothIcon className="h-5 w-5" aria-hidden="true" />
             </div>
             <div>Settings</div>
-            <div className="ml-auto h-5 w-5 flex-shrink-0 text-gray-400">
-              <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+            <div className="ml-auto h-4 w-4 flex-shrink-0">
+              <ChevronDownIcon className="h-4 w-4" aria-hidden="true" />
             </div>
           </a>
         </div>
