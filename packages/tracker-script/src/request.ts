3a2baf889b677631fefd318835775f50e31e50c5
nsylke <nicksylke@outlook.com>
2023-10-02 17:35:17 -0500

    refactor: handling requests in tracker

--- packages/tracker-script/src/request.ts
+++ packages/tracker-script/src/request.ts
@@ -1,23 +1,34 @@
 import { baseUrl } from "./config";
 
 export function handleRequest(siteId: string, payload: string) {
-  if (!("fetch" in window)) {
-    if (typeof navigator.sendBeacon === "function") {
-      navigator.sendBeacon(`${baseUrl}${siteId}`, payload);
-    } else {
-      const img = new Image();
-      img.src = `${baseUrl}${siteId}?p=${payload}`;
+  for (const handle of [postFetch, postBeacon]) {
+    if (handle(`${baseUrl}${siteId}`, payload)) {
+      break;
     }
-  } else {
-    fetch(`${baseUrl}${siteId}`, {
-      method: "post",
-      body: payload,
-      credentials: "omit",
-      cache: "no-store",
-      mode: "no-cors",
-      headers: {
-        "content-type": "text/plain",
-      },
-    });
   }
+}
+
+function postFetch(url: string, payload: string): boolean {
+  if (!("fetch" in window)) return false;
+
+  fetch(url, {
+    method: "post",
+    body: payload,
+    credentials: "omit",
+    cache: "no-store",
+    mode: "no-cors",
+    headers: {
+      "content-type": "text/plain",
+    },
+  });
+
+  return true;
+}
+
+function postBeacon(url: string, payload: string): boolean {
+  if (typeof navigator.sendBeacon !== "function") return false;
+
+  navigator.sendBeacon(url, payload);
+
+  return true;
 }
