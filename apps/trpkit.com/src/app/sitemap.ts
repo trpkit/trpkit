a595f44605ea10cce76ae2680bcf0c1aee85d3db
nsylke <nicksylke@outlook.com>
2024-03-24 17:07:48 -0400

    fix: remove lingering contentlayer in sitemap

--- apps/trpkit.com/src/app/sitemap.ts
+++ apps/trpkit.com/src/app/sitemap.ts
@@ -1,21 +1,10 @@
 import { MetadataRoute } from "next";
-import { allBlogDocuments, allLegalDocuments } from "contentlayer/generated";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const legalPages = allLegalDocuments.map((doc) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL || "https://trpkit.com"}/${doc.href}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
-
-  const blogPages = allBlogDocuments.map((doc) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL || "https://trpkit.com"}/${doc.href}`,
-    lastModified: doc.date,
-  }));
-
   const routes = ["", "/blog", "/transparency"].map((route) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL || "https://trpkit.com"}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogPages, ...legalPages];
+  return [...routes];
 }
