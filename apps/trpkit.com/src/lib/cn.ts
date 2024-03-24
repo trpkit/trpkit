43243e75c73272898e73cdbba671f29afbbda92e
nsylke <nicksylke@outlook.com>
2024-03-24 17:25:49 -0400

    feat: use clsx & tailwind-merge for cn function

--- apps/trpkit.com/src/lib/cn.ts
+++ apps/trpkit.com/src/lib/cn.ts
@@ -1,3 +1,6 @@
-export function cn(...classes: any[]) {
-  return classes.filter(Boolean).join(" ");
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
