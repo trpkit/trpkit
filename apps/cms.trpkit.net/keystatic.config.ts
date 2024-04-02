dba383d8985e73765ede31951691375b80a45c53
nsylke <nicksylke@outlook.com>
2024-04-02 15:05:40 -0500

    feat: create basic author schema

--- apps/cms.trpkit.net/keystatic.config.ts
+++ apps/cms.trpkit.net/keystatic.config.ts
@@ -1,10 +1,32 @@
-import { config } from "@keystatic/core";
+import { collection, config, fields } from "@keystatic/core";
 
 export default config({
   storage: {
     kind: "local",
+    // repo: "trpkit/materials",
+    // branchPrefix: "",
   },
   ui: {
     brand: { name: "Trpkit" },
+  },
+  collections: {
+    authors: collection({
+      label: "Authors",
+      path: "author/*",
+      slugField: "name",
+      schema: {
+        name: fields.slug({
+          name: {
+            label: "Name",
+            validation: {
+              length: {
+                min: 1,
+              },
+            },
+          },
+        }),
+        title: fields.text({ label: "Title" }),
+      },
+    }),
   },
 });
