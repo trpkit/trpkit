20a430d3dcbc7921415235b2e918b3822d62a79f
nsylke <nick@trpkit.com>
2024-04-02 16:57:04 -0500

    feat: implement current collections from trpkit/materials repository

--- apps/cms.trpkit.net/keystatic.config.ts
+++ apps/cms.trpkit.net/keystatic.config.ts
@@ -2,30 +2,63 @@ import { collection, config, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
-    // repo: "trpkit/materials",
-    // branchPrefix: "",
-  },
-  ui: {
-    brand: { name: "Trpkit" },
+    kind: "github",
+    repo: {
+      owner: "trpkit",
+      name: "materials",
+    },
   },
   collections: {
-    authors: collection({
-      label: "Authors",
-      path: "author/*",
+    author: collection({
+      label: "Blog authors",
       slugField: "name",
+      path: "author/*",
       schema: {
-        name: fields.slug({
-          name: {
-            label: "Name",
-            validation: {
-              length: {
-                min: 1,
-              },
-            },
-          },
-        }),
+        name: fields.slug({ name: { label: "Name" } }),
         title: fields.text({ label: "Title" }),
+      },
+    }),
+    blog: collection({
+      label: "Blog posts",
+      slugField: "title",
+      path: "blog/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        publishedDate: fields.date({ label: "Published date" }),
+        authors: fields.array(
+          fields.relationship({
+            label: "Author",
+            collection: "author",
+          }),
+          {
+            label: "Authors",
+            validation: { length: { min: 1 } },
+            itemLabel: (props) => props.value || "Please select an author",
+          }
+        ),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+        }),
+      },
+    }),
+    legal: collection({
+      label: "Legal documents",
+      slugField: "title",
+      path: "legal/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        effectiveDate: fields.date({ label: "Effective date" }),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+        }),
       },
     }),
   },
--- apps/cms.trpkit.net/src/app/keystatic/layout.tsx
+++ apps/cms.trpkit.net/src/app/keystatic/layout.tsx
@@ -1,5 +1,11 @@
 import KeystaticApp from "./keystatic";
 
 export default function Layout() {
-  return <KeystaticApp />;
+  return (
+    <html>
+      <body>
+        <KeystaticApp />
+      </body>
+    </html>
+  );
 }
