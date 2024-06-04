c78c263e271f5759520644db73df1f888e4956b5
nsylke <nick@trpkit.com>
2024-06-03 20:09:48 -0500

    refactor: remove cms.trpkit.net package

--- apps/cms.trpkit.net/src/app/api/keystatic/[...params]/route.ts
+++
@@ -1,6 +0,0 @@
-import { makeRouteHandler } from "@keystatic/next/route-handler";
-import config from "../../../../../keystatic.config";
-
-export const { POST, GET } = makeRouteHandler({
-  config,
-});
