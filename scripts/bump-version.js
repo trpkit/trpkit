9ce9102f226c67c9aee11e511ef3eb4ef5224aa4
nsylke <nick@trpkit.com>
2024-06-03 20:10:45 -0500

    refactor: cleanup version bump script

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -2,48 +2,20 @@ const pkg = require("../package.json");
 const glob = require("glob");
 const fs = require("fs");
 
-// Updating Node.js packages
-glob.sync("./apps/**/package.json").forEach(
-  (loc) =>
-    !loc.includes("node_modules") &&
-    fs.writeFileSync(
-      loc,
-      JSON.stringify(
-        {
-          ...JSON.parse(fs.readFileSync(loc)),
-          version: pkg.version,
-        },
-        null,
-        2
+["./apps/**/package.json", "./packages/**/package.json"].forEach((dir) =>
+  glob.sync(dir).forEach(
+    (loc) =>
+      !loc.includes("node_modules") &&
+      fs.writeFileSync(
+        loc,
+        JSON.stringify(
+          {
+            ...JSON.parse(fs.readFileSync(loc)),
+            version: pkg.version,
+          },
+          null,
+          2
+        )
       )
-    )
+  )
 );
-
-glob.sync("./packages/**/package.json").forEach(
-  (loc) =>
-    !loc.includes("node_modules") &&
-    fs.writeFileSync(
-      loc,
-      JSON.stringify(
-        {
-          ...JSON.parse(fs.readFileSync(loc)),
-          version: pkg.version,
-        },
-        null,
-        2
-      )
-    )
-);
-
-// Updating Rust packages
-glob.sync("./apps/**/Cargo.toml").forEach((loc) => {
-  const data = fs.readFileSync(loc, "utf-8");
-  const newData = data.replace(/(\[package\][^\[]*version\s*=\s*)"[^"]*"/, `$1"${pkg.version}"`);
-  fs.writeFileSync(loc, newData);
-});
-
-glob.sync("./packages/**/Cargo.toml").forEach((loc) => {
-  const data = fs.readFileSync(loc, "utf-8");
-  const newData = data.replace(/(\[package\][^\[]*version\s*=\s*)"[^"]*"/, `$1"${pkg.version}"`);
-  fs.writeFileSync(loc, newData);
-});
