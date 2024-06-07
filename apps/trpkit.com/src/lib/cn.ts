774625a50b4797f272f71979c030cc4a228b9bb0
nsylke <nick@trpkit.com>
2024-06-06 22:38:31 -0500

    docs: write documentation for cn function

--- apps/trpkit.com/src/lib/cn.ts
+++ apps/trpkit.com/src/lib/cn.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines class names using clsx and merges Tailwind CSS classes using twMerge
+ *
+ * @param {...ClassValue[]} inputs - A list of class name values
+ * @returns {string} The resulting string of combined and merged class names
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
