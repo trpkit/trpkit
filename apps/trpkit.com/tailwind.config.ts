e6c1253bfddb6edbc6922a16aad2509a3df4745a
nsylke <nick@trpkit.com>
2024-06-06 19:47:58 -0500

    refactor: disable turbo & convert tailwind config to ts

--- apps/trpkit.com/tailwind.config.ts
+++ apps/trpkit.com/tailwind.config.ts
@@ -1,10 +1,13 @@
-/**
- * @type {import('tailwindcss').Config}
- */
-module.exports = {
+import { Config } from "tailwindcss";
+
+export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-inter)"],
@@ -16,4 +19,4 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
   ],
-};
+} satisfies Config;
