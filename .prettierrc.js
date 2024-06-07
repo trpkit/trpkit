d82f4d6e5e66798cf542f996c876b8c80424ef30
nsylke <nick@trpkit.com>
2024-06-06 23:28:25 -0500

    refactor: move KeystaticApp to separate folder for all Keystatic/Markdoc related files

--- .prettierrc.js
+++ .prettierrc.js
@@ -16,6 +16,7 @@ module.exports = {
     "^@/app/(.*)$",
     "^@/components/(.*)$",
     "^@/hooks/(.*)$",
+    "^@/keystatic/(.*)$",
     "^@/lib/(.*)$",
     "^@/styles/(.*)$",
     "^[./]",
--- apps/trpkit.com/src/app/(keystatic)/keystatic/layout.tsx
+++ apps/trpkit.com/src/app/(keystatic)/keystatic/layout.tsx
@@ -1,4 +1,4 @@
-import KeystaticApp from "./keystatic";
+import KeystaticApp from "@/keystatic/KeystaticApp";
 
 export default function Layout() {
   return (
