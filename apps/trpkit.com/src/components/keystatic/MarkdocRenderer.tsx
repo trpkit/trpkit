659e87c7720451df307f4a29e4d0eab5f404cac5
nsylke <nick@trpkit.com>
2024-09-06 19:14:32 -0500

    feat: create TextLink component & configure in keystatic

--- apps/trpkit.com/src/components/keystatic/MarkdocRenderer.tsx
+++ apps/trpkit.com/src/components/keystatic/MarkdocRenderer.tsx
@@ -1,7 +1,6 @@
 import { Divider } from "@/components/ui/Divider";
 import { Heading } from "@/components/ui/Heading";
-import { Link } from "@/components/ui/Link";
-import { Strong, Text } from "@/components/ui/Text";
+import { Strong, Text, TextLink } from "@/components/ui/Text";
 import { markdocTransform } from "@/keystatic/markdoc-transform";
 import { type Node, renderers } from "@markdoc/markdoc";
 import React, { Fragment, type ReactNode } from "react";
@@ -23,7 +22,7 @@ function getRenderers() {
   return {
     Fragment,
     Link: ({ href, children }: { href: string; children: string }) => (
-      <Link href={href}>{children}</Link>
+      <TextLink href={href}>{children}</TextLink>
     ),
     Heading: ({ level, children }: { level: 1 | 2 | 3 | 4 | 5 | 6; children: ReactNode }) => (
       <Heading level={level}>{children}</Heading>
--- apps/trpkit.com/src/components/ui/Text.tsx
+++ apps/trpkit.com/src/components/ui/Text.tsx
@@ -1,8 +1,21 @@
+import { Link } from "@/components/ui/Link";
 import { cn } from "@/lib/cn";
 import type React from "react";
 
 export function Text({ className, ...props }: React.ComponentPropsWithoutRef<"p">) {
   return <p {...props} className={cn(className, "text-base/6 text-zinc-700 sm:text-sm/6")} />;
+}
+
+export function TextLink({ className, ...props }: React.ComponentPropsWithoutRef<typeof Link>) {
+  return (
+    <Link
+      {...props}
+      className={cn(
+        className,
+        "text-zinc-950 underline decoration-zinc-950/50 data-[hover]:decoration-zinc-950"
+      )}
+    />
+  );
 }
 
 export function Strong({ className, ...props }: React.ComponentPropsWithoutRef<"strong">) {
