a02678d9b2b8dae1ea1ef0f73fd246d3f85de986
nsylke <nick@trpkit.com>
2024-09-10 20:31:49 -0500

    feat: add touch target component

---
+++ apps/trpkit.com/src/components/ui/TouchTarget.tsx
@@ -0,0 +1,13 @@
+import type React from "react";
+
+export function TouchTarget({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <span
+        className="absolute left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
+        aria-hidden="true"
+      />
+      {children}
+    </>
+  );
+}
