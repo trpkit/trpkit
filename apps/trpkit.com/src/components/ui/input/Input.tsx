958e330615de5648f91625f930146fe117a4bb68
nsylke <nick@trpkit.com>
2024-09-11 21:15:45 -0500

    feat: create input components

---
+++ apps/trpkit.com/src/components/ui/input/Input.tsx
@@ -0,0 +1,62 @@
+import { cn } from "@/lib/cn";
+import * as Headless from "@headlessui/react";
+import type React from "react";
+import { forwardRef } from "react";
+
+const dateTypes = ["date", "datetime-local", "month", "time", "week"];
+type DateType = (typeof dateTypes)[number];
+
+type Props = {
+  className?: string;
+  type?: "email" | "number" | "password" | "search" | "tel" | "text" | "url" | DateType;
+} & Omit<Headless.InputProps, "as" | "className">;
+
+export const Input = forwardRef(function Input(
+  { className, ...props }: Props,
+  ref: React.ForwardedRef<HTMLInputElement>
+) {
+  return (
+    <span
+      data-slot="control"
+      className={cn([
+        className,
+        "relative block w-full",
+        "before:absolute before:inset-px before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-white before:shadow",
+        "dark:before:hidden",
+        "after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-inset after:ring-transparent sm:after:focus-within:ring-2 sm:after:focus-within:ring-blue-500",
+        "has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-zinc-950/5 before:has-[[data-disabled]]:shadow-none",
+        "before:has-[[data-invalid]]:shadow-red-500/10",
+      ])}
+    >
+      <Headless.Input
+        ref={ref}
+        {...props}
+        className={cn([
+          props.type &&
+            dateTypes.includes(props.type) && [
+              "[&::-webkit-datetime-edit-fields-wrapper]:p-0",
+              "[&::-webkit-date-and-time-value]:min-h-[1.5em]",
+              "[&::-webkit-datetime-edit]:inline-flex",
+              "[&::-webkit-datetime-edit]:p-0",
+              "[&::-webkit-datetime-edit-year-field]:p-0",
+              "[&::-webkit-datetime-edit-month-field]:p-0",
+              "[&::-webkit-datetime-edit-day-field]:p-0",
+              "[&::-webkit-datetime-edit-hour-field]:p-0",
+              "[&::-webkit-datetime-edit-minute-field]:p-0",
+              "[&::-webkit-datetime-edit-second-field]:p-0",
+              "[&::-webkit-datetime-edit-millisecond-field]:p-0",
+              "[&::-webkit-datetime-edit-meridiem-field]:p-0",
+            ],
+          "relative block w-full appearance-none rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)]",
+          "text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white",
+          "border border-zinc-950/10 data-[hover]:border-zinc-950/20 dark:border-white/10 dark:data-[hover]:border-white/20",
+          "bg-transparent dark:bg-white/5",
+          "focus:outline-none",
+          "data-[invalid]:border-red-500 data-[invalid]:data-[hover]:border-red-500 data-[invalid]:dark:border-red-500 data-[invalid]:data-[hover]:dark:border-red-500",
+          "data-[disabled]:border-zinc-950/20 dark:data-[hover]:data-[disabled]:border-white/15 data-[disabled]:dark:border-white/15 data-[disabled]:dark:bg-white/[2.5%]",
+          "dark:[color-scheme:dark]",
+        ])}
+      />
+    </span>
+  );
+});
