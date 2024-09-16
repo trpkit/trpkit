9157a0af15d6ec1784c90deb1b66b1ba22e43618
nsylke <nick@trpkit.com>
2024-09-15 20:18:40 -0500

    refactor: remove the prefixed env variables from storage package

--- packages/storage/src/mongo.ts
+++ packages/storage/src/mongo.ts
@@ -4,11 +4,11 @@ let _mongo: MongoClient | null = null;
 
 export async function mongo(): Promise<MongoClient> {
   if (!_mongo) {
-    if (!process.env.TRPKIT_MONGO_URI) {
-      throw new Error("@trpkit/storage: Missing required environment variable TRPKIT_MONGO_URI");
+    if (!process.env.MONGO_URI) {
+      throw new Error("@trpkit/storage: Missing required environment variable MONGO_URI");
     }
 
-    _mongo = new MongoClient(process.env.TRPKIT_MONGO_URI, {});
+    _mongo = new MongoClient(process.env.MONGO_URI, {});
     await _mongo.connect();
   }
 
--- packages/storage/src/r2.ts
+++ packages/storage/src/r2.ts
@@ -5,21 +5,21 @@ let _r2: S3Client | null = null;
 export async function r2(): Promise<S3Client> {
   if (!_r2) {
     if (
-      !process.env.TRPKIT_R2_ACCOUNT ||
-      !process.env.TRPKIT_R2_ACCESS_ID ||
-      !process.env.TRPKIT_R2_ACCESS_KEY
+      !process.env.R2_ACCOUNT ||
+      !process.env.R2_ACCESS_ID ||
+      !process.env.R2_ACCESS_KEY
     ) {
       throw new Error(
-        "@trpkit/storage: Missing required environment variables TRPKIT_R2_ACCOUNT, TRPKIT_R2_ACCESS_ID and TRPKIT_R2_ACCESS_KEY"
+        "@trpkit/storage: Missing required environment variables R2_ACCOUNT, R2_ACCESS_ID and R2_ACCESS_KEY"
       );
     }
 
     _r2 = new S3Client({
       region: "auto",
-      endpoint: `https://${process.env.TRPKIT_R2_ACCOUNT}.r2.cloudflarestorage.com`,
+      endpoint: `https://${process.env.R2_ACCOUNT}.r2.cloudflarestorage.com`,
       credentials: {
-        accessKeyId: process.env.TRPKIT_R2_ACCESS_ID,
-        secretAccessKey: process.env.TRPKIT_R2_ACCESS_KEY,
+        accessKeyId: process.env.R2_ACCESS_ID,
+        secretAccessKey: process.env.R2_ACCESS_KEY,
       },
     });
   }
