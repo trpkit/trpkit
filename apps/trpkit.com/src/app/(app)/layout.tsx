996b7a11b6ef6acae024439b3b80c7fb43cd7ad6
nsylke <nick@trpkit.com>
2024-09-18 21:44:05 -0500

    fix: remove html/body from auth & app

--- apps/trpkit.com/src/app/(app)/layout.tsx
+++ apps/trpkit.com/src/app/(app)/layout.tsx
@@ -1,9 +1,5 @@
 import type React from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
