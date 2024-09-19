62bfc534c7d0311c99d12a09155444ed2b7d7efa
nsylke <nick@trpkit.com>
2024-09-18 19:11:07 -0500

    refactor: remove components that won't be used after/in soft launch

--- apps/trpkit.com/src/app/(marketing)/page.tsx
+++ apps/trpkit.com/src/app/(marketing)/page.tsx
@@ -1,15 +1,9 @@
-import Faq from "@/components/marketing/Faq";
-import Features from "@/components/marketing/Features";
-import Waitlist from "@/components/marketing/Waitlist";
 import Hero from "@/components/marketing/home/Hero";
 
 export default function Page() {
   return (
     <>
       <Hero />
-      <Features />
-      <Faq />
-      <Waitlist />
     </>
   );
 }
