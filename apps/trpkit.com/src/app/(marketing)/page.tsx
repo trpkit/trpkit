f34d2b8e0cd14253456d219bc47683e1cd477bb0
nsylke <nick@trpkit.com>
2024-09-19 21:55:14 -0500

    refactor: remove sub folder in marketing components

--- apps/trpkit.com/src/app/(marketing)/page.tsx
+++ apps/trpkit.com/src/app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/marketing/home/Hero";
-import WebsiteMonitoring from "@/components/marketing/home/WebsiteMonitoring";
+import Hero from "@/components/marketing/Hero";
+import WebsiteMonitoring from "@/components/marketing/WebsiteMonitoring";
 import type React from "react";
 
 export default function Page() {
