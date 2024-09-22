f4b3748aaa0c7366063774fa310aa7e0d2cc259d
nsylke <nick@trpkit.com>
2024-09-21 22:17:45 -0500

    chore: run formatter

--- apps/trpkit.com/src/components/marketing/WebsiteMonitoring.tsx
+++ apps/trpkit.com/src/components/marketing/WebsiteMonitoring.tsx
@@ -6,7 +6,9 @@ export default function WebsiteMonitoring() {
     <section className="overflow-hidden pt-32">
       <div className="space-y-8 md:!space-y-16 container">
         <div className="text-left mx-0">
-          <Subheading className="sm:text-4xl mb-3 md:mb-6">Zero-config website and SSL monitoring</Subheading>
+          <Subheading className="sm:text-4xl mb-3 md:mb-6">
+            Zero-config website and SSL monitoring
+          </Subheading>
           <Text className="sm:text-xl">
             View response timings of your site from around the world and ensure the validity of your
             SSL certificate.
